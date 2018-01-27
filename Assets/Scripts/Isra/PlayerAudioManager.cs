a9c0d0226870d2b18e841f5ff13501c55f5c735e
IsraelNebot <israel.nebot@gmail.com>
2018-01-27 05:00:40 +0100

    Added sounds to player

--- Assets/Scripts/Isra/PlayerAudioManager.cs
+++ Assets/Scripts/Isra/PlayerAudioManager.cs
@@ -5,5 +5,28 @@ using UnityEngine;
 public class PlayerAudioManager : MonoBehaviour {
 
 	public AudioClip[] m_PlayerAudioClips;
+	public AudioSource m_PlayerJumpAudioSource;
+	public AudioSource m_PlayerDoubleJumpAudioSource;
+	public AudioSource m_PlayerDeathAudioSource;
+	public AudioSource m_PlayerScreamAudioSource;
+
+
+	public void PlayJumpSound(){
+		m_PlayerJumpAudioSource.Play();
+	}
+
+	public void PlayDoubleJumpSound(){
+		m_PlayerDoubleJumpAudioSource.Play();
+	}
+
+	public void PlayDeathSound(){
+		m_PlayerDeathAudioSource.Play();
+	}
+
+	public void PlayScreamSound(){
+		m_PlayerScreamAudioSource.Play();
+	}
+
+
 
 }
