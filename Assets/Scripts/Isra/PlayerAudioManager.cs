4157928c0937f5c5cccf17904e3f98f941e16f4a
IsraelNebot <israel.nebot@gmail.com>
2018-01-27 04:10:33 +0100

    Camera works properly, started implementing player sounds w/ class that holds them

---
+++ Assets/Scripts/Isra/PlayerAudioManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudioManager : MonoBehaviour {
+
+	public AudioClip[] m_PlayerAudioClips;
+
+}
