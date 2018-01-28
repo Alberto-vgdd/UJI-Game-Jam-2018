08dbc1fe36432ea126ed8486bf2acd9ba59f9be8
Albert Dols Ferrandis <al315151@uji.es>
2018-01-28 01:38:11 +0100

    Start Game casi hecho
    Faltaria que fuera llamada la función al hacer la acción deseada.

---
+++ Assets/Scripts/Albert/StartGameBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGameBehaviour : MonoBehaviour {
+
+	public GameObject idleBedAnimation;
+
+	public GameObject startGameBedAnimation;
+
+	public GameObject playerReference;
+
+	public static StartGameBehaviour currentInstance;
+
+	void Awake()
+	{
+		currentInstance = this;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+		startGameBedAnimation.SetActive (false);
+		idleBedAnimation.SetActive (true);
+		playerReference.SetActive (false);
+
+	}
+
+	public void StartGameAnimation()
+	{
+		idleBedAnimation.SetActive (false);
+		startGameBedAnimation.SetActive (true);
+		playerReference.SetActive (true);
+	}
+
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			StartGameBehaviour.currentInstance.StartGameAnimation ();
+		}
+	}
+}
